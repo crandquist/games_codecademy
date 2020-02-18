b6fc65ae52a10eb11e7ba1bd1830705a845d3431
Madi <crandquist@gmail.com>
2020-02-17 16:01:24 -0800

    Removed return statements from end of coin_flip and added a helper function try_again.

--- games.py
+++ games.py
@@ -6,14 +6,35 @@ money = 100
 def pick_game():
     pass
 
+def balance():
+    print("You have {0} dollars.".format(money))
+
+def try_again(game):
+    y_or_n = print("Would you like to try again? Enter 'y' or 'n'")
+    if y_or_n == "y":
+        game()
+    elif y_or_n == "n":
+        print("Thank you for playing!")
+        new_game = input("Would you like to play another game? Enter 'y' or 'n'.")
+        if new_game == "y":
+            pick_game()
+        elif new_game == "n":
+            return (print "See you next time!")
+        else:
+            print("Whoops. Please enter 'y' or 'n'.")
+            try_again(game)
+
+
 def coin_flip():
     print("Welcome to coin flip!")
     print("In this game you will decide whether you want to bet heads or tails and how much money you are betting on.")
     print("If the coin flip results in your favor you will win back your bet.")
     print("If it does not, you will lose that money.")
     print("Here we go!")
+    
     wager = bet()
     int_wager = int(wager)
+    
     h_or_t = heads_or_tails()
     print("Time to flip the coin!")
     flip = random.randint(1, 2)
@@ -21,12 +42,16 @@ def coin_flip():
         print("Heads!")
     elif flip == 2:
         print("Tails!")
+    
     if h_or_t == flip:
         print("Congratulations! You won {0} dollars!".format(wager))
-        return int_wager
+        money += int_wager
+        balance()
+
     else:
         print("You lost {0} dollars. Better luck next time!".format(wager))
-        return -int_wager
+        money -= int_wager
+        print("You have {0} dollars!".format(money))
     
 
     #Helper function for coin_flip game.
@@ -51,9 +76,28 @@ def cho_han():
     print("In this game we will be rolling two dice and adding the results together. ")
     print("If you can guess correctly if the number is even or odd then you win!")
     print("Let's begin!")
+    
     wager = bet()
     int_wager = int(wager)
     e_or_o = input("Do you bet the result is going to be even or odd?")
+    
+    dice_1 = dice_roll()
+    print("The first dice roll was {0}".format(dice_1))
+    dice_2 = dice_roll()
+    print("The second dice roll was {0}".format(dice_2))
+    total_roll = dice_1 + dice_2
+    even = None
+    
+    if total_roll % 2 is 0:
+        even = True
+    else:
+        even = False
+    if (even and e_or_o == "even") or (not even and e_or_o == "odd"):
+        print("Congratulations! You won {0} dollars!".format(int_wager))
+    else:
+        print("You lost {0} dollars. Better luck next time!".format(int_wager))
+
+    
 
 
 def dice_roll():
